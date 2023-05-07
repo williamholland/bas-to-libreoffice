987e87e675c91a4c5ff07efb9bf4cc94f1217859
Will Holland <william.dai.holland@outlook.com>
2023-05-07 14:48:45 +0700

    get the parameters from the commandline

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import os
+import argparse
 from jinja2 import Environment, FileSystemLoader
+import xml.sax.saxutils
 
 # Set up the Jinja2 environment to load templates from the LibraryTemplate directory
 env = Environment(loader=FileSystemLoader('LibraryTemplate'))
 
+# Set up the command-line argument parser
+parser = argparse.ArgumentParser(description='Make a LibreOffice library from source')
+parser.add_argument('--lib-name', dest='lib_name', required=True, help='name of the library')
+parser.add_argument('--module-name', dest='module_name', required=True, help='name of the module')
+parser.add_argument('--module-path', dest='module_path', required=True, help='path to the module .bas file')
+
+args = parser.parse_args()
+
 # Define the variables to use in the templates
-lib_name = 'Library2'
-module_name = 'Module2'
-module_code = '''
-Sub Main
-    MsgBox "Hello world"
-End Sub
-'''
+lib_name = args.lib_name
+module_name = args.module_name
+# Read the module file
+with open(args.module_path, 'r') as f:
+    module_code = f.read()
+    module_code = xml.sax.saxutils.escape(module_code)
 
 # Create a new directory to hold the rendered templates
 output_dir = lib_name
